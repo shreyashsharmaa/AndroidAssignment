f1dd7bb97ce5fd1294550e6e615581ed26e3f9f7
shreyash sharma <shreyash1703@gmail.com>
2018-10-02 00:23:17 +0530

    signup

--- app/src/main/java/com/example/shreyash/quizzler/SignUpActivity.java
+++ app/src/main/java/com/example/shreyash/quizzler/SignUpActivity.java
@@ -1,13 +1,38 @@
 package com.example.shreyash.quizzler;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        button1 =(Button)findViewById(R.id.loginButton);
+            button1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openHomeActivity();
+                }
+            });
+
+
+        }
+
+        public void openHomeActivity()
+        {
+            Intent intent = new Intent(this,HomeActivity.class);
+            startActivity(intent);
+        }
+
+
+
     }
-}
+
+
+
